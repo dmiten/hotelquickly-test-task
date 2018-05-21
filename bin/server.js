72080852025f03149f5884ebbc9cacab7e73108c
Dmitry Tenitskiy <netlan@yandex.ru>
2018-05-21 09:06:34 +0700

    Socket server

--- bin/server.js
+++ bin/server.js
@@ -6,40 +6,46 @@ const express = require('express');
 
 require('../lib/db');
 
+const app = require('../lib/app');
 const config = require('../lib/config');
 const log = require('../lib/log')(module);
-const app = require('../lib/app');
+const socketServer = require('../lib/socketServer');
 
 const getCredentials = require('../config/.data/credentials');
-let credentials;
 
-if (config.get('useHttps')) {
-  credentials = getCredentials();
-}
+const credentials = config.get('useHttps') ? getCredentials() : null;
+
+let server;
 
 if (credentials) {
+  app.set('port', process.env.PORT || config.get('port:https') || 3443);
+
   const httpApp = express();
   httpApp.set('port', process.env.PORT || config.get('port:http') || 3000);
-  app.set('port', process.env.PORT || config.get('port:https') || 3443);
 
   httpApp.get('*', (req, res) => {
     res.redirect('https://' + req.hostname + ':' + app.get('port') + req.url);
     log.info('Redirected to HTTPS');
   });
 
-  http.createServer(httpApp).listen(httpApp.get('port'), () => {
-    log.info('Express HTTP server listening on port ' + httpApp.get('port'));
-  });
+  http.createServer(httpApp)
+    .listen(httpApp.get('port'), () =>
+      log.info('Express HTTP server listening on port ' + httpApp.get('port'))
+    );
 
-  https.createServer(credentials, app).listen(app.get('port'), () => {
-    log.info('Express HTTPS server listening on port ' + app.get('port'));
-  });
+  server = https.createServer(credentials, app);
+
+  server.listen(app.get('port'), () =>
+    log.info('Express HTTPS server listening on port ' + app.get('port'))
+  );
 } else {
   app.set('port', process.env.PORT || config.get('port:http') || 3000);
 
-  const server = http.createServer(app);
+  server = http.createServer(app);
 
-  server.listen(app.get('port'), () => {
-    log.info('Express HTTP server listening on port ' + app.get('port'));
-  });
+  server.listen(app.get('port'), () =>
+    log.info('Express HTTP server listening on port ' + app.get('port'))
+  );
 }
+
+socketServer.create(server);
